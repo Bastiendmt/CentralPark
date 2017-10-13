9728dc8d010c14ca606a8db4f93b9396fb44817b
bastiendmt <bastienxs@gmail.com>
2017-10-13 17:26:21 +0200

    Tout les noms 0

--- script.js
+++ script.js
@@ -16,10 +16,23 @@ jQuery(document).ready(function(){
         $(this).css("background-color", "pink");
     }); */
 
-    $.get( "https://swapi.co/api/people/1/?format=json", function( data ) {
-  $( ".result" ).html( data ),data.name;
-  console.log( "Load was performed,", "data.name = ", data.name );
-  $("#Dynamique h2").text(data.name);
-});
+    $.get( "https://swapi.co/api/people/1/?format=json", function( data ) { //Affichage du nom
+        $( ".result" ).html( data ),data['name']; //Marche aussi avec data.name
+        console.log( "Load was performed,", "data.name = ", data['name'] );
+        $("#Dynamique h2").text(data.name); 
+    });
+
+
+    https://swapi.co/api/people/?format=json
+
+    $.get("https://swapi.co/api/people/?format=json", function( data) {
+        //$(".result").html(data), data['results'];
+        console.log("Loading all names =" );
+        //console.log(data['results']) = Liste de tout les personnages 
+        //console.log(data['results']['0']); //Liste des détails d'un personnage (0)
+        for (elm in data['results']) {
+            console.log(data['results'][elm].name);
+        }
+    });
 
 });
